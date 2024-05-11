1771c531561c891bea0781179f83494a76209456
KOSASIH <kosasihg88@gmail.com>
2024-05-11 14:29:28 +0700

    Create object_detection.py

---
+++ computer_vision/object_detection.py
@@ -0,0 +1,12 @@
+import cv2
+
+def detect_objects(image, config):
+    # Load the object detection model
+    net = cv2.dnn.readNetFromDarknet(config['model_config'], config['model_weights'])
+
+    # Perform object detection on the image
+    blob = cv2.dnn.blobFromImage(image, config['input_scale'], config['input_size'], config['mean_subtraction'])
+    net.setInput(blob)
+    detections = net.forward()
+
+    return detections
