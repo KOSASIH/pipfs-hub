4ff9525a3221f87df62e51114efd99989e2401dd
KOSASIH <kosasihg88@gmail.com>
2024-05-11 14:41:21 +0700

    Create gpio.py

---
+++ hardware_interface/gpio.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+
+def setup_gpio():
+    # Set up GPIO pins using RPi.GPIO or gpiozero
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.cleanup()
+
+def set_gpio_mode(pin, mode):
+    # Set the mode of a GPIO pin
+
+    GPIO.setup(pin, mode)
+
+def set_gpio_value(pin, value):
+    # Set the value of a GPIO pin
+
+    GPIO.output(pin, value)
+
+def get_gpio_value(pin):
+    # Get the value of a GPIO pin
+
+    return GPIO.input(pin)
