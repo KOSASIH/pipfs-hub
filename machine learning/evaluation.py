caac62504166d65d566f2cc2a405f27eed8855da
KOSASIH <kosasihg88@gmail.com>
2024-05-11 14:25:45 +0700

    Delete machine learning directory

--- machine learning/evaluation.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def evaluate_model(model, data, labels):
-    # Evaluate the performance of the trained model
-    loss, metric = model.evaluate(data, labels)
-
-    return loss, metric
--- machine learning/prediction.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def predict(model, data):
-    # Make predictions using the trained model
-    predictions = model.predict(data)
-
-    return predictions
