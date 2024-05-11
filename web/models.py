77f302725232eef9ec99e7fc2c280ede885d8cc1
KOSASIH <kosasihg88@gmail.com>
2024-05-11 13:48:28 +0700

    Create models.py

---
+++ web/models.py
@@ -0,0 +1,13 @@
+# web/models.py
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Pipfs(Base):
+    __tablename__ = 'pipfs'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    # ...
